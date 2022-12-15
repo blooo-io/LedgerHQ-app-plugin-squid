58145bb1e76238d49754fe6d501bff67c7c11948
Z4karia <92750334+Z4karia@users.noreply.github.com>
2022-12-15 11:56:33 +0000

    refactor(finalize): removed unused function

--- src/handle_finalize.c
+++ src/handle_finalize.c
@@ -5,11 +5,6 @@ static void sent_network_token(squid_parameters_t *context) {
     context->tokens_found |= TOKEN_SENT_FOUND;
 }
 
-static void received_network_token(squid_parameters_t *context) {
-    context->decimals_received = WEI_TO_ETHER;
-    context->tokens_found |= TOKEN_RECEIVED_FOUND;
-}
-
 static bool set_ticker_for_mapped_token(squid_parameters_t *context) {
     for (size_t i = 0; i < NUM_SUPPORTED_TOKENS; i++) {
         if (!memcmp(context->token_symbol,
@@ -17,7 +12,7 @@ static bool set_ticker_for_mapped_token(squid_parameters_t *context) {
                     MAX_TICKER_LEN)) {
             char ticker[MAX_TICKER_LEN];
             strlcpy(ticker, (char *) SQUID_SUPPORTED_TOKENS[i].token_symbol, sizeof(ticker));
-            strcat(ticker, " ");
+            strlcat(ticker, " ", sizeof(ticker));
             strlcpy(context->ticker_sent, (char *) ticker, sizeof(context->ticker_sent));
             context->decimals_sent = SQUID_SUPPORTED_TOKENS[i].decimals_sent;
             context->tokens_found |= TOKEN_SENT_FOUND;
@@ -72,17 +67,6 @@ void handle_finalize(void *parameters) {
             // Add a warning screen if the dest chain is not supported
             msg->numScreens++;
         }
-
-        // if (!ADDRESS_IS_NETWORK_TOKEN(context->token_received)) {
-        //     // Address is not network token (0xeee...) so we will look up the token in the
-        //     CAL. printf_hex_array("Setting address received to: ",
-        //                      ADDRESS_LENGTH,
-        //                      context->token_received);
-        //     msg->tokenLookup2 = context->token_received;
-        // } else {
-        //     received_network_token(context);
-        //     msg->tokenLookup2 = NULL;
-        // }
 
         msg->uiType = ETH_UI_TYPE_GENERIC;
         msg->result = ETH_PLUGIN_RESULT_OK;
