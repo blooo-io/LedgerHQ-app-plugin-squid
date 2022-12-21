eeef2351bac1135ada2998cb0bc40d6bd6e99195
Z4karia <92750334+Z4karia@users.noreply.github.com>
2022-12-21 16:46:21 +0000

    test(callBridgeCall): removed to_asset screen

--- tests/src/bsc/bsc_callBridgeCall_chain_warning.test.js
+++ tests/src/bsc/bsc_callBridgeCall_chain_warning.test.js
@@ -22,17 +22,17 @@ const devices = [
     {
         name: "nanos",
         label: "Nano S",
-        steps: 8, // <= Define the number of steps for this test case and this device
+        steps: 7, // <= Define the number of steps for this test case and this device
     },
     {
         name: "nanox",
         label: "Nano X",
-        steps: 8, // <= Define the number of steps for this test case and this device
+        steps: 7, // <= Define the number of steps for this test case and this device
     },
     {
         name: "nanosp",
         label: "Nano S+",
-        steps: 8, // <= Define the number of steps for this test case and this device
+        steps: 7, // <= Define the number of steps for this test case and this device
     }
 ];
 
